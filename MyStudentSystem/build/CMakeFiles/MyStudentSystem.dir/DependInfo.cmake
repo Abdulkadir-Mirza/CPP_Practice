a3b4c5a677893682405c999831ed69e95ad347a5
Abdulkadir-eInfo <abdulkadir.mirza@einfochips.com>
2023-02-21 12:00:03 +0530

    StudentManagementSystem

---
+++ MyStudentSystem/build/CMakeFiles/MyStudentSystem.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/akm009/Desktop/C++Practice/MyRepository/MyStudentSystem.cpp" "/home/akm009/Desktop/C++Practice/MyRepository/build/CMakeFiles/MyStudentSystem.dir/MyStudentSystem.cpp.o"
+  "/home/akm009/Desktop/C++Practice/MyRepository/src/Base.cpp" "/home/akm009/Desktop/C++Practice/MyRepository/build/CMakeFiles/MyStudentSystem.dir/src/Base.cpp.o"
+  "/home/akm009/Desktop/C++Practice/MyRepository/src/DisplayOperations.cpp" "/home/akm009/Desktop/C++Practice/MyRepository/build/CMakeFiles/MyStudentSystem.dir/src/DisplayOperations.cpp.o"
+  "/home/akm009/Desktop/C++Practice/MyRepository/src/Operations.cpp" "/home/akm009/Desktop/C++Practice/MyRepository/build/CMakeFiles/MyStudentSystem.dir/src/Operations.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MyStudentSystem/build/CMakeFiles/MyStudentSystem.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyStudentSystem.dir/MyStudentSystem.cpp.o"
+  "CMakeFiles/MyStudentSystem.dir/src/Base.cpp.o"
+  "CMakeFiles/MyStudentSystem.dir/src/DisplayOperations.cpp.o"
+  "CMakeFiles/MyStudentSystem.dir/src/Operations.cpp.o"
+  "MyStudentSystem"
+  "MyStudentSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyStudentSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
